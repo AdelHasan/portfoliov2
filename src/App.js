3051d69e5aa082f30e7a2d1a5d7282c0857d3a1f
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-13 18:17:36 -0500

    embed google drive pdf into resume component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 import { Container, Col, Row } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
       </Container>
       <Footer />
---
+++ src/components/Resume.js
@@ -0,0 +1,7 @@
+const Resume = () => {
+  return (
+    <div><iframe src="https://drive.google.com/file/d/111kpFVPBEtph__uEhUYXz3CAbjT16L5t/preview" width="640" height="480" allow="autoplay"></iframe></div>
+  )
+}
+
+export default Resume
