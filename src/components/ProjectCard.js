0e1f0f9f188ded68000ae50662eea3be43129197
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-13 01:40:16 -0600

    projects cards rendered with props

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,19 +2,20 @@
 import { Card } from "react-bootstrap";
 
 // *------ P R O J E C T  C A R D ------*
-const ProjectCard = () => {
+const ProjectCard = ({name, description, repoLink, deployedLink, techStack}) => {
   return (
     <Card
       style={{ width: "18rem" }}
       className="text-center d-flex justify-content-center"
     >
-      <Card.Header>Project Name</Card.Header>
+      <Card.Header>{name}</Card.Header>
       <Card.Img variant="top" src="https://picsum.photos/200" />
       <Card.Body>
-        <Card.Title className="mb-2 text-muted">Tech Stack</Card.Title>
-        <Card.Text>Project Description</Card.Text>
-        <Card.Link href="#">Repo</Card.Link>
-        <Card.Link href="#">Deployed</Card.Link>
+        <Card.Title>Technologies:</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{techStack}</Card.Subtitle>
+        <Card.Text>{description}</Card.Text>
+        <Card.Link href={repoLink}>Repo</Card.Link>
+        <Card.Link href={deployedLink}>Deployed</Card.Link>
       </Card.Body>
     </Card>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,64 @@
+// *------ I M P O R T S ------*
 import ProjectCard from "./ProjectCard";
 import { Col, Row } from "react-bootstrap";
 // *---------------------------*
+
+// *------ P R O J E C T S  I N F O ------*
+const project1 = {
+  name: 'Portfolio',
+  description: 'A portfolio site',
+  repoLink: 'https://github.com/AdelHasan',
+  deployedLink: 'https://github.com/AdelHasan',
+  techStack: 'HTML, CSS, JQuery, Bootstrap'
+}
+
+const project2 = {
+  name: 'Research Launchpad',
+  description: 'A react app for Google Books API',
+  repoLink: 'https://github.com/AdelHasan',
+  deployedLink: 'https://github.com/AdelHasan',
+  techStack: 'React, Bootstrap, Async/Await'
+}
+
+const project3 = {
+  name: 'GarageSale',
+  description: 'An e-commerce site',
+  repoLink: 'https://github.com/AdelHasan',
+  deployedLink: 'https://github.com/AdelHasan',
+  techStack: 'MERN Stack'
+}
+
 // *------ P R O J E C T S ------*
 const Projects = () => {
   return (
     <div>
       <Row>
         <Col>
-          <ProjectCard />
+          <ProjectCard
+            name={project1.name}
+            description={project1.description}
+            repoLink={project1.repoLink}
+            deployedLink={project1.deployedLink}
+            techStack={project1.techStack} 
+          />
         </Col>
         <Col>
-          <ProjectCard />
+        <ProjectCard
+            name={project2.name}
+            description={project2.description}
+            repoLink={project2.repoLink}
+            deployedLink={project2.deployedLink}
+            techStack={project2.techStack} 
+          />
         </Col>
         <Col>
-          <ProjectCard />
+        <ProjectCard
+            name={project3.name}
+            description={project3.description}
+            repoLink={project3.repoLink}
+            deployedLink={project3.deployedLink}
+            techStack={project3.techStack} 
+          />
         </Col>
       </Row>
     </div>
